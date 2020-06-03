d85659570e49f788f994981c355876943d276d39
Michal Pawluczuk <mi23chal@gmail.com>
2020-06-03 23:41:46 +0200

    feat: add index & 404 pages

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SEO from 'components/SEO';
+
+const NotFoundPage = () => (
+	<>
+		<SEO title="404: Strony nie odnaleziono" />
+		<h1>404: Strony nie odnaleziono</h1>
+	</>
+);
+
+export default NotFoundPage;
