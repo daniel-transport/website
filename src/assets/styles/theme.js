5cc3047e616cac0eae3807e141c2d829b218b307
Michal Pawluczuk <mi23chal@gmail.com>
2020-06-04 00:08:12 +0200

    feat: add theme config

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+	color: {
+		dark: '#1d2739',
+		bright: '#f2f2f2',
+		primary: '#009920',
+		transparentBlack: 'rgba(0, 0, 0, 0.85)',
+	},
+	mq: {
+		mobileMid: '@media (min-width: 23.5rem)',
+		tablet: '@media (min-width: 37.5rem)',
+		tabletMid: '@media (min-width: 42rem)',
+		tabletBig: '@media (min-width: 56.25rem)',
+		desktop: '@media (min-width: 75rem)',
+		desktopBig: '@media (min-width: 112.5rem)',
+	},
+};
+
+export default theme;
