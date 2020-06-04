1c5983c8079a03923239a14e19774df503e8db80
Michal Pawluczuk <mi23chal@gmail.com>
2020-06-04 15:46:33 +0200

    build: add smoothScroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,30 @@
 /* eslint react/prop-types:0 */
-/* eslint import/prefer-default-export: 0 */
+/* eslint consistent-return:0 */
 import './src/assets/styles/global.css';
 import './src/assets/styles/fonts.css';
 
 import React from 'react';
 import Layout from './src/layouts/index';
+import { getOffset, smoothScroll } from './src/utils/index';
 
 const wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
 
-export { wrapPageElement };
+const shouldUpdateScroll = (
+	{ routerProps: { location }, prevRouterProps },
+	opts = {},
+) => {
+	const { offsetElement = '#navbar', duration = 1000 } = opts;
+
+	if (!!location.hash && !!prevRouterProps) {
+		const destinationElement = document.querySelector(location.hash);
+		if (!destinationElement) return false;
+
+		const destination = destinationElement.getBoundingClientRect().top;
+		const offset = getOffset(offsetElement);
+
+		smoothScroll(destination, offset, duration);
+		return false;
+	}
+};
+
+export { wrapPageElement, shouldUpdateScroll };
